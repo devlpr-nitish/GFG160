e2771966aff9ba3e502bdd08667e27762c6d19fd
Nitish <nk3227610@gmail.com>
2024-12-04 19:14:06 +0530

    done

--- 04_12_2024.cpp
+++ 04_12_2024.cpp
@@ -2,6 +2,10 @@ class Solution {
   public:
     // Function to check if two strings are rotations of each other or not.
     bool areRotations(string &s1, string &s2) {
+        
+        if(s1.length() != s2.length()){
+            return false;
+        }
 
         string final = s1 + s1;
         
